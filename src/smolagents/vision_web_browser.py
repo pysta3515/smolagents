c28a26f4d9cc4d4101df0026c4a35eeca8fd1834
Amir Mahla <ammah@Amirs-MacBook-Pro.local>
2025-06-02 19:34:39 +0200

    test h integration

--- src/smolagents/vision_web_browser.py
+++ src/smolagents/vision_web_browser.py
@@ -3,15 +3,19 @@ from io import BytesIO
 from time import sleep
 
 import helium
+import os
 import PIL.Image
+from PIL import ImageDraw
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from helium import go_to
 
-from smolagents import CodeAgent, WebSearchTool, tool
+from smolagents import ToolCallingAgent, WebSearchTool, tool
 from smolagents.agents import ActionStep
 from smolagents.cli import load_model
+from smolagents.models import InferenceClientModel
 
 
 github_request = """
@@ -23,6 +27,12 @@ search_request = """
 Please navigate to https://en.wikipedia.org/wiki/Chicago and give me a sentence containing the word "1992" that mentions a construction accident.
 """
 
+meteo_request = """
+Please navigate to a weather website and tell me the current weather conditions and forecast for Paris, France.
+Include the temperature, precipitation chances, and general weather conditions for today.
+"""
+
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Run a web browser automation script with a specified model.")
@@ -30,7 +40,7 @@ def parse_arguments():
         "prompt",
         type=str,
         nargs="?",  # Makes it optional
-        default=search_request,
+        default=meteo_request,
         help="The prompt to run with the agent",
     )
     parser.add_argument(
@@ -63,7 +73,7 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def save_screenshot(memory_step: ActionStep, agent: CodeAgent) -> None:
+def save_screenshot(memory_step: ActionStep, agent: ToolCallingAgent) -> None:
     sleep(1.0)  # Let JavaScript animations happen before taking the screenshot
     driver = helium.get_driver()
     current_step = memory_step.step_number
@@ -82,6 +92,7 @@ def save_screenshot(memory_step: ActionStep, agent: CodeAgent) -> None:
         url_info if memory_step.observations is None else memory_step.observations + "\n" + url_info
     )
     return
+
 
 
 @tool
@@ -107,9 +118,71 @@ def go_back() -> None:
     """Goes back to previous page."""
     driver.back()
 
+@tool
+def click(x: int, y: int) -> None:
+    """
+    Click on a position on the screen
+    
+    Args:
+        x: num pixels from the left edge
+        y: num pixels from the top edge
+    """
+    # Reset mouse position first
+    from helium import click as helium_click
+    import helium
+    driver = helium.get_driver()
+    scroll_x = driver.execute_script("return window.scrollX;")
+    scroll_y = driver.execute_script("return window.scrollY;")
+    loc = helium.Point(scroll_x + x, scroll_y + y)
+    helium_click(loc)
+
+
 
 @tool
-def close_popups() -> str:
+def go_to(page: str) -> None:
+    """
+    Navigate to a webpage.
+    
+    Args:
+        page: URL of the page to navigate to
+    """
+    from helium import go_to as helium_go_to
+    helium_go_to(page)
+
+@tool
+def type_text(text: str) -> None:
+    """
+    Type text into the currently focused element.
+    
+    Args:
+        text: The text to type
+    """
+    webdriver.ActionChains(driver).send_keys(text).perform()
+
+
+@tool
+def scroll_page_down() -> None:
+    """
+    Scroll down the page by one page length.
+    
+    Args:
+        num_pixels: Number of pixels to scroll down
+    """
+    webdriver.ActionChains(driver).send_keys(Keys.PAGE_DOWN).perform()
+
+@tool
+def scroll_page_up() -> None:
+    """
+    Scroll up the page by one page length.
+    
+    Args:
+        num_pixels: Number of pixels to scroll down
+    """
+    webdriver.ActionChains(driver).send_keys(Keys.PAGE_UP).perform()
+
+
+@tool
+def close_popups() -> None:
     """
     Closes any visible modal or pop-up on the page. Use this to dismiss pop-up windows! This does not work on cookie consent banners.
     """
@@ -128,10 +201,9 @@ def initialize_driver():
 
 def initialize_agent(model):
     """Initialize the CodeAgent with the specified model."""
-    return CodeAgent(
-        tools=[WebSearchTool(), go_back, close_popups, search_item_ctrl_f],
+    return ToolCallingAgent(
+        tools=[go_to, type_text, scroll_page_down, scroll_page_up, click, close_popups, search_item_ctrl_f],
         model=model,
-        additional_authorized_imports=["helium"],
         step_callbacks=[save_screenshot],
         max_steps=20,
         verbosity_level=2,
@@ -143,54 +215,55 @@ Use your web_search tool when you want to get Google search results.
 Then you can use helium to access websites. Don't use helium for Google search, only for navigating websites!
 Don't bother about the helium driver, it's already managed.
 We've already ran "from helium import *"
-Then you can go to pages!
-Code:
-```py
-go_to('github.com/trending')
-```<end_code>
+First, go to the relevant page using go_to:
+Action:
+{
+    "name": "go_to",
+    "arguments": {"page": "github.com/trending"}
+}
 
-You can directly click clickable elements by inputting the text that appears on them.
-Code:
-```py
-click("Top products")
-```<end_code>
+You can directly click clickable elements by inputting a click position as Click(x, y) with x num pixels from the left edge and y num pixels from the top edge.
+Action:
+{
+    "name": "click",
+    "arguments": {"x": 352, "y": 348}
+}
 
-If it's a link:
-Code:
-```py
-click(Link("Top products"))
-```<end_code>
+To type text into a field after click on, use type_text:
+Action:
+{
+    "name": "type_text", 
+    "arguments": {"text": "Hello world"}
+}
+
+
 
 If you try to interact with an element and it's not found, you'll get a LookupError.
 In general stop your action after each button click to see what happens on your screenshot.
 Never try to login in a page.
 
 To scroll up or down, use scroll_down or scroll_up with as an argument the number of pixels to scroll from.
-Code:
-```py
-scroll_down(num_pixels=1200) # This will scroll one viewport down
-```<end_code>
+Action:
+{
+    "name": "scroll_down",
+    "arguments": {"num_pixels": 1200}
+}
 
 When you have pop-ups with a cross icon to close, don't try to click the close icon by finding its element or targeting an 'X' element (this most often fails).
 Just use your built-in tool `close_popups` to close them:
-Code:
-```py
-close_popups()
-```<end_code>
-
-You can use .exists() to check for the existence of an element. For example:
-Code:
-```py
-if Text('Accept cookies?').exists():
-    click('I accept')
-```<end_code>
+Action:
+{
+    "name": "close_popups",
+    "arguments": {}
+}
 
 Proceed in several steps rather than trying to solve the task in one shot.
 And at the end, only when you have your answer, return your final answer.
-Code:
-```py
-final_answer("YOUR_ANSWER_HERE")
-```<end_code>
+Action:
+{
+    "name": "final_answer",
+    "arguments": "YOUR_ANSWER_HERE"
+}
 
 If pages seem stuck on loading, you might have to wait, for instance `import time` and run `time.sleep(5.0)`. But don't overuse this!
 To list elements on page, DO NOT try code-based element searches like 'contributors = find_all(S("ol > li"))': just look at the latest screenshot you have and read it visually, or use your tool search_item_ctrl_f.
@@ -199,6 +272,8 @@ After each code blob you write, you will be automatically provided with an updat
 But beware that the screenshot will only be taken at the end of the whole action, it won't see intermediate states.
 Don't kill the browser.
 When you have modals or cookie banners on screen, you should get rid of them before you can click anything else.
+Always output a single action at a time, using ONLY the format shown below.
+
 """
 
 
@@ -212,16 +287,20 @@ def run_webagent(
 ) -> None:
     # Load environment variables
     load_dotenv()
+    os.makedirs("screenshots", exist_ok=True)
 
     # Initialize the model based on the provided arguments
-    model = load_model(model_type, model_id, provider=provider, api_base=api_base, api_key=api_key)
+    # model = load_model(model_type, model_id, provider=provider, api_base=api_base, api_key=api_key)
+    model = InferenceClientModel(
+        model_id="https://oa9u6uq0urded73f.us-east-1.aws.endpoints.huggingface.cloud",
+        max_tokens=4096,
+    )
 
     global driver
     driver = initialize_driver()
     agent = initialize_agent(model)
 
     # Run the agent with the provided prompt
-    agent.python_executor("from helium import *")
     agent.run(prompt + helium_instructions)
 
 
