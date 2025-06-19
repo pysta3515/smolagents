0b2d836bdc84ef1837c5a0997b04c88277768f67
Aymeric <aymeric.roucher@gmail.com>
2025-06-19 16:33:45 +0200

    Reset gradio example

--- examples/gradio_ui.py
+++ examples/gradio_ui.py
@@ -1,15 +1,16 @@
-from smolagents import CodeAgent, GradioUI, TransformersModel, WebSearchTool
+from smolagents import CodeAgent, GradioUI, InferenceClientModel, WebSearchTool
 
 
 agent = CodeAgent(
     tools=[WebSearchTool()],
-    model=TransformersModel(model_id="HuggingFaceTB/SmolLM2-1.7B-Instruct"),
+    model=InferenceClientModel(model_id="meta-llama/Llama-3.3-70B-Instruct", provider="fireworks-ai"),
     verbosity_level=1,
-    # planning_interval=3,
+    planning_interval=3,
     name="example_agent",
     description="This is an example agent.",
     step_callbacks=[],
     stream_outputs=True,
+    use_structured_outputs_internally=True,
 )
-print(agent.initialize_system_prompt())
+
 GradioUI(agent, file_upload_folder="./data").launch()
